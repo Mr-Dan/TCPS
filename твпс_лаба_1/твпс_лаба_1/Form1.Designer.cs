e942ae0fbf53c7999906bca4bcceee343614981b
Mr-Dan <35195104+Mr-Dan@users.noreply.github.com>
2022-04-05 07:59:38 +0300

    Add files via upload

---
+++ твпс_лаба_1/твпс_лаба_1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace твпс_лаба_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(766, 185);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "bccbd\nbccbdbccbd\nbccbdbccbdbccbdbccbd\nfbcgcbgdf\nbcc bd \nbdd\nbdddd \nbccccccc\nbcbdd" +
+    "dbx\nbbbbb\nbfbdsb\nbbbcc\nbbdcc";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 239);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(332, 199);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(358, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "(bcc)+|(bd*)+";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(431, 239);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(366, 199);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Castom Regex";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(725, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Regex Library";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ твпс_лаба_1/твпс_лаба_1/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace твпс_лаба_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            string reg = "(bcc)+|(bd*)+";
+            string text = "bccbdddbccdsvbbbdddasewjhgbcbccc";
+            text = richTextBox1.Text+" ";
+            // text = "bb";
+            List<string> res =  castom(text);
+           foreach(string test in res)
+            {
+                richTextBox2.Text = richTextBox2.Text + test.Trim('\n') + '\n';
+            }
+            regex(text);
+        }
+
+        private List<string> castom(string text)
+        {
+            List<string> list = new List<string>();
+            int state = 1;
+            int start = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                switch(state)
+                {
+                    case 1:
+                        switch(text[i])
+                        {
+                            case 'b': state = 2; break;
+                            default:
+                                list.Add(text.Substring(start, i - start));
+                              state = 1;
+                              start = i + 1;
+                             break;
+                        }
+                    break;
+                    case 2:
+                        switch(text[i])
+                        {
+                            case 'b': state = 3; break;
+                            case 'd': state = 3; break;
+                            case 'c': state = 4; break;
+                            default:
+                                state = 1;
+                                start = i + 1; 
+                                break;
+                        }
+                    break;
+                    case 3:
+                        switch (text[i])
+                        {
+                            case 'b':  break;
+                            case 'd':  break;
+                            default:
+                                list.Add(text.Substring(start, i - start));
+                                state = 1;
+                                start = i + 1;
+                                break;
+                        }
+                    break;
+                    case 4:
+                        switch(text[i])
+                        {                          
+                            case 'c': state = 5;break;                        
+                            default:
+
+                                state = 1;
+                                start = i + 1;
+                                break;
+                        }
+                    break;
+                    case 5:                      
+                        switch (text[i])
+                        {
+                            case 'b': state = 6; break;
+                            default:
+                                list.Add(text.Substring(start, i - start));
+                                state = 1;
+                                start = i + 1;
+                             break;
+                        }
+                        list.Add(text.Substring(start, i - start));
+                        break;
+                    case 6:
+                        switch (text[i])
+                        {
+                            case 'c': state = 4; break;
+                            default:
+                                state = 1;
+                                start = i + 1; 
+                                break;
+                        }
+                    break;
+                }
+
+
+            }
+            return list;
+        }
+        private void castomReg(string text)
+        {
+            int countB = 0;
+            int countC = 0;
+            int countD = 0;
+            int pos = 0;
+            while (true)
+            {
+
+                switch (text[pos])
+                {
+                    case 'b':
+                        countB++;
+                    
+                        if (text[pos + 1] != 'd'  && text[pos + 1] != 'b')
+                        {
+                         
+                            if (checkPattern(text, pos) == false)
+                            {
+                                if (countB == 1)
+                                {
+                                    richTextBox2.Text = richTextBox2.Text  + text.Substring(pos, 1) + '\n';
+                                    countB = 0;
+                                }
+                                else if (countB > 1 && countD == 0)
+                                {
+                                    richTextBox2.Text = richTextBox2.Text  + text.Substring(pos - countB+1, countB) + '\n';
+                                    countB = 0;
+                                }
+                                else if(countD > 0)
+                                {
+                                    richTextBox2.Text = richTextBox2.Text + text.Substring(pos - countD - countB + 1, countD + countB) + '\n';
+                                    countB = 0;
+                                    countD = 0;
+                                }                            
+                            }
+                            else
+                            {
+                              
+                                if  (countD > 0)
+                                {
+                                    richTextBox2.Text = richTextBox2.Text  + text.Substring(pos - countD - countB + 1, countD + countB) + '\n';
+                                    countB = 0;
+                                    countD = 0;
+                                }
+                                else if (countB > 1 && countD == 0)
+                                {
+                                    richTextBox2.Text = richTextBox2.Text + text.Substring(pos - countB + 1, countB) + '\n';
+                                    countB = 0;
+                                }
+                            }
+
+
+                        }
+
+                        break;
+
+                    case 'c':
+                        countC++;
+                        if (countC == 2 && countB == 1)
+                        {
+                            richTextBox2.Text = richTextBox2.Text + text.Substring(pos - 2, 3) + '\n';
+                            countB = 0;
+                            countC = 0;
+                        }
+                        if (text[pos + 1] != 'c')
+                        {
+                            countB = 0;
+                            countC = 0;
+                        }
+                        break;
+
+                    case 'd':
+                       if(countB>0) countD++;
+                        if (text[pos + 1] != 'd' && text[pos + 1] != 'b' && countB >0)
+                        {
+                            richTextBox2.Text = richTextBox2.Text + text.Substring(pos - countD- countB+1, countD+ countB) + '\n';
+                            countB = 0;
+                            countD = 0;
+                        }
+                       
+                        break;
+
+                    default:
+                        countB = 0;
+                        countC = 0;
+                        countD = 0;
+                        break;
+
+                }
+                pos++;
+                if (pos > text.Length - 1) break;
+
+            }
+        }
+        bool checkPattern(string text,int pos)
+        {
+            if(pos+3 <= text.Length-1)
+            {
+                if (text[pos + 1] == 'c' && (text[pos + 2] == 'c')) return true;
+                else return false;
+            }
+            else return false;
+        }
+        private void regex(string text)
+        {
+           
+            Regex regex = new Regex("(bcc)+|(bd*)+");
+            MatchCollection matchCollection = regex.Matches(text);
+            if (matchCollection.Count > 0)
+            {
+                foreach (Match match in matchCollection)
+                    richTextBox3.Text = richTextBox3.Text  + match.Value + '\n';
+            }
+        }
+    } 
+
+    /*
+#include <iostream>
+#include <string>
+using namespace std;
+bool result(string text);
+    int main()
+    {//a(b|c|d)c
+        string text = "abcdc";
+        cout << result(text);
+        return 0;
+    }
+
+    bool result(string text)
+    {
+        int state = 1;
+        for (int i = 0; i < text.size(); i++)
+        {
+            switch (state)
+            {
+                case 1:
+                    switch (text[i])
+                    {
+                        case 'a': state = 2; break;
+                        default: return false;
+                    }
+                    break;
+                case 2:
+                    switch (text[i])
+                    {
+                        case 'b': break;
+                        case 'd': break;
+                        case 'c': state = 3; break;
+                        default: return false;
+                    }
+
+                    break;
+
+                case 3:
+
+                    switch (text[i])
+                    {
+                        case 'b': state = 2; break;
+                        case 'd': state = 2; break;
+                        case 'c': break;
+                        default: return false;
+                    }
+                    break;
+            }
+        }
+        if (state == 3) return true;
+        else return false;
+    }
+
+
+     */
+
+}
